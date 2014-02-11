e892e7ed929c960c38888d264ac232a465436176
Chris <chris.haack@me.com>
2014-02-11 08:10:44 -0500

    add method to format graffiti data as an array of hashes for heatmap

--- app/models/graffiti.rb
+++ app/models/graffiti.rb
@@ -3,24 +3,36 @@ require 'open-uri'
 
 class Graffiti < ActiveRecord::Base
 
-  attr_reader :get_graffiti
+  attr_accessible :get_graffiti, :incident_address, :x_coordinate, :y_coordinate
+
+  geocoded_by :incident_address, :if => :incident_address_changed?
+  after_validation :geocode
+
+  def self.geocoded_graffiti
+    geo_data = []
+    Graffiti.by_location.each do |incident|
+      geo_data << { lat:incident.x_coordinate, lng:incident.y_coordinate}
+  end
+
 
   def get_graffiti
     data = open("http://data.cityofnewyork.us/resource/gpwd-npar.json")
-    parsed = JSON.parse(data.read)
-    parsed.each do |graffiti_report|
-      graffiti_report.delete("created_date")
-      graffiti_report.delete("bbl")
-      graffiti_report.delete("city_council_district")
-      graffiti_report.delete("closed_date")
-      graffiti_report.delete("resolution_action")
-      graffiti_report.delete("community_board")
-      graffiti_report.delete("police_precinct")
-      g = Graffiti.new(graffiti_report)
+    graffiti_parsed = JSON.parse(data.read)
+    graffiti_parsed.each do |incident|
+      incident.delete("created_date")
+      incident.delete("bbl")
+      incident.delete("city_council_district")
+      incident.delete("closed_date")
+      incident.delete("resolution_action")
+      incident.delete("community_board")
+      incident.delete("police_precinct")
+      g = Graffiti.new(incident)
       g.save
     end
-    parsed
+    graffiti_parsed
   end
+
+
 
 
 end

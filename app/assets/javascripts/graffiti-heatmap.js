2daa6d614302bbded946fb1f9e992960da520d0e
Chris <chris.haack@me.com>
2014-02-12 13:55:23 -0500

    add function to count dbl clicks and show markers, add buttons and functions to show markers

--- app/assets/javascripts/graffiti-heatmap.js
+++ app/assets/javascripts/graffiti-heatmap.js
@@ -1,16 +1,18 @@
-var map;
-var mapOptions;
-var heatmap;
-var GraffitiData;
-var myLatlng;
-var mapData;
+var map, heatLayer, bikeLayer;
+var markers = [];
 
 window.onload = function () {
   myLatlng = new google.maps.LatLng(40.715845, -73.884675);
   mapOptions = {
     center: myLatlng,
-    zoom: 11
+    zoom: 11,
+    streetViewControl: true,
+    zoomControlOptions: {
+      style: google.maps.ZoomControlStyle.SMALL
+
+    }
   };
+
   map = new google.maps.Map(document.getElementById("map-canvas"),
     mapOptions);
 
@@ -37,12 +39,48 @@ window.onload = function () {
       var bikeLayer = new google.maps.BicyclingLayer();
       bikeLayer.setMap(map);
       $.each(mapData.data, function (i, g) {
-        layer2 = new google.maps.Marker({
+        marker = new google.maps.Marker({
           position: new google.maps.LatLng(g.lat, g.lng),
           map: map,
           zIndex: 100
         });
+        markers.push(marker);
+        clearMarkers();
       });
     }
   });
 };
+
+
+// Sets the map on all markers in the array.
+function setAllMap(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setAllMap(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setAllMap(map);
+}
+
+
+$(document).ready(function () {
+
+  var i = 0;
+  $('#map-canvas').on("dblclick", function () {
+    console.log("markers");
+    if (i < 1) {
+      clearMarkers();
+    } else {
+      showMarkers();
+    }
+    i++;
+  });
+
+});

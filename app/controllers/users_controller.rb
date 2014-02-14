c5500ee6ec2c462cd75e92ce3eab24322b4a81fd
Chris <chris.haack@me.com>
2014-02-14 07:33:59 -0500

    add create method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def create
+    #get user data from form
+    @user = User.new(params[:user])
+    @user.save
+      flash[:success] = "Let's find some graffiti."
+      redirect_to user_path(@user)
+    else
+      render "new"
+    end
   end
 end

63501185cbb9dd42899e4410f01ce3388d95afe8
Chris <chris.haack@me.com>
2014-02-16 13:15:24 -0500

    clean up controller js

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -10,157 +10,149 @@ var marker;
 
 
 graffitiApp.controller('GraffitiListCtrl', ['$scope', '$http', 'Graffiti',
-    
-  function($scope, $http, Graffiti) {
-    
-    $http.get('graffiti/get_graffiti.json').success(function(data) {
+
+  function ($scope, $http, Graffiti) {
+
+    $http.get('graffiti/get_graffiti.json').success(function (data) {
       $scope.graffitis = data;
     });
 
-    $http.get('graffiti/geo_graffiti.json').success(function(data) {
-    $scope.geo_graffiti = data;
-    var mapData = {
+    $http.get('graffiti/geo_graffiti.json').success(function (data) {
+      $scope.geo_graffiti = data;
+      var mapData = {
         //max is the abruptness of the gradient
         max: 10,
         data: data
       };
 
-    var mapOptions = {
-      center: queens,
-      zoom: 10,
-      streetViewControl: true,
-      zoomControlOptions: {
-        style: google.maps.ZoomControlStyle.SMALL
-      },
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      disableDefaultUI: false,
-      scrollwheel: true,
-      draggable: true,
-      navigationControl: true,
-      mapTypeControl: false,
-      scaleControl: true,
-      disableDoubleClickZoom: false
-    };
+      var mapOptions = {
+        center: queens,
+        zoom: 10,
+        streetViewControl: true,
+        zoomControlOptions: {
+          style: google.maps.ZoomControlStyle.SMALL
+        },
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: false,
+        scrollwheel: true,
+        draggable: true,
+        navigationControl: true,
+        mapTypeControl: false,
+        scaleControl: true,
+        disableDoubleClickZoom: false
+      };
 
-    map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
+      map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
 
-    var heatmap = new HeatmapOverlay(map, {
-      "radius": 16,
-      "dissipating": false,
-      "visible": true,
-      "opacity": 80
-    });
+      var heatmap = new HeatmapOverlay(map, {
+        "radius": 16,
+        "dissipating": false,
+        "visible": true,
+        "opacity": 80
+      });
 
-    var heatLayer = google.maps.event.addListener(map, "idle", function () {
+      var heatLayer = google.maps.event.addListener(map, "idle", function () {
         heatmap.setDataSet(mapData);
       });
 
-    $.each(mapData.data, function (i, g) {
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(g.lat, g.lng),
-        map: map,
-        zIndex: 100,
-        icon: "http://maps.google.com/mapfiles/dir_39.png"
-      });
-      markers.push(marker);
-      clearMarkers();
-      (function (marker, i) {
-        // add click event
-        google.maps.event.addListener(marker, 'click', function () {
-        panorama = map.getStreetView();
-        panorama.setPosition(marker.getPosition());
-        panorama.setPov({
-        heading: 265,
-        pitch: 0
+      $.each(mapData.data, function (i, g) {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(g.lat, g.lng),
+          map: map,
+          zIndex: 100,
+          icon: "http://maps.google.com/mapfiles/dir_39.png"
         });
-        panorama.setVisible(true);
-        });
-      })(marker, i);
-    });
-
-    // Sets the map on all markers in the array.
-    function setAllMap(map) {
-      for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-      }
-    }
-
-    //get latitude of each datum
-    function matchLat(map, lat) {
-      for (var i = 0; i < markers.length; i++) {
-        if (markers[i].position.d == lat) {
-          console.log(markers[i]);
-          panorama = map.getStreetView();
-          panorama.setPosition(markers[i].getPosition());
-          panorama.setPov({
-          heading: 265,
-          pitch: 0
+        markers.push(marker);
+        clearMarkers();
+        (function (marker, i) {
+          // add click event
+          google.maps.event.addListener(marker, 'click', function () {
+            panorama = map.getStreetView();
+            panorama.setPosition(marker.getPosition());
+            panorama.setPov({
+              heading: 265,
+              pitch: 0
+            });
+            panorama.setVisible(true);
           });
-          panorama.setVisible(true);
-        }          
-      }
-    }
+        })(marker, i);
+      });
 
-    // Removes the markers from the map, but keeps them in the array.
-    function clearMarkers() {
-      setAllMap(null);
-    }
-
-    // Shows any markers currently in the array.
-    function showMarkers() {
-      setAllMap(map);
-    }
-
-    function toggleMarkers(marker, map) {
-      if (marker.getMap() == null)
-          setAllMap(map); // marker isn't visible on map, so make it visible
-      else
-          setAllMap(null); // marker is visible on map, so make it invisible
-    }
-
-    $(document).ready(function () {
-      
-      var i = 0;
-
-      $('#map-canvas').on("dblclick", function () {
-        if (i < 2) {
-          clearMarkers();
-        } else {
-          showMarkers();
+      // Sets the map on all markers in the array.
+      function setAllMap(map) {
+        for (var i = 0; i < markers.length; i++) {
+          markers[i].setMap(map);
         }
-        i++;
-      });
-      
-      // click on address, retrieve latitude
-      //search for marker with that latitude
-      //open streetview in google map
+      }
 
-    //   $('ul .address').click(function(){
-    //     alert(setTimeout("$(this).find('li').eq(2).text().match(/\d{2}.\d+/).pop()", 1500));
-    //     // alert(lat);
-    //     matchLat(map, lat);
-    // });
+      //get latitude of each datum
+      function matchLat(map, lat) {
+        for (var i = 0; i < markers.length; i++) {
+          if (markers[i].position.d == lat) {
+            console.log(markers[i]);
+            panorama = map.getStreetView();
+            panorama.setPosition(markers[i].getPosition());
+            panorama.setPov({
+              heading: 265,
+              pitch: 0
+            });
+            panorama.setVisible(true);
+          }
+        }
+      }
 
-      $('ul .address').on('click', function(event) {   
-        setTimeout(1200);  
-        var lat = $(this).find('li').eq(2).text().match(/\d{2}.\d+/).pop();
-        matchLat(map, lat);
-      });
+      // Removes the markers from the map, but keeps them in the array.
+      function clearMarkers() {
+        setAllMap(null);
+      }
 
-      $('#markers').on("click", function() {
-        toggleMarkers(marker, map);
-      });
+      // Shows any markers currently in the array.
+      function showMarkers() {
+        setAllMap(map);
+      }
 
-      $('#heat').on("click", function() {
-        heatmap.toggle();
+      function toggleMarkers(marker, map) {
+        if (marker.getMap() == null)
+          setAllMap(map); // marker isn't visible on map, so make it visible
+        else
+          setAllMap(null); // marker is visible on map, so make it invisible
+      }
+
+      $(document).ready(function () {
+
+        var i = 0;
+
+        $('#map-canvas').on("dblclick", function () {
+          if (i < 2) {
+            clearMarkers();
+          } else {
+            showMarkers();
+          }
+          i++;
+        });
+
+        // click on address, open streetview in google map
+        $('ul .address').on('click', function (event) {
+          setTimeout(1200);
+          var lat = $(this).find('li').eq(2).text().match(/\d{2}.\d+/).pop();
+          matchLat(map, lat);
+        });
+
+        $('#markers').on("click", function () {
+          toggleMarkers(marker, map);
+        });
+
+        $('#heat').on("click", function () {
+          heatmap.toggle();
+        });
+
       });
 
     });
-      
-  });
-$scope.orderProp = 'incident_address';
-}]);
+    $scope.orderProp = 'incident_address';
+  }
+]);
 
 
 

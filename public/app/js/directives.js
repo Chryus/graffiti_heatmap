9a1e1bc42d02ea229bbd3d8a978f5dc2c4511b7b
Chris <chris.haack@me.com>
2016-08-11 11:12:48 -0400

    deprecate

--- public/app/js/directives.js
+++ public/app/js/directives.js
@@ -1,22 +1,22 @@
-'use strict';
+// 'use strict';
 
-graffitiApp.directive('graffitiDashboard', function (){
-  var width = 960,
-     height = 500;
+// graffitiApp.directive('graffitiDashboard', function (){
+//   var width = 960,
+//      height = 500;
 
-  return {
-    restrict: 'E',
-    scope: {
-      graffitis: '=',
-    },
-    link: function (scope, element, attrs) {
-      var dash = d3.select("body")
-        .append("svg")
-          .attr("width", width)
-          .attr("height", height);
+//   return {
+//     restrict: 'E',
+//     scope: {
+//       graffitis: '=',
+//     },
+//     link: function (scope, element, attrs) {
+//       var dash = d3.select("body")
+//         .append("svg")
+//           .attr("width", width)
+//           .attr("height", height);
 
-      scope.$watch('graffitis', function (newVal, oldVal)
-    }
+//       scope.$watch('graffitis', function (newVal, oldVal)
+//     }
 
 
-})
+// })

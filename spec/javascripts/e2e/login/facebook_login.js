fc59c257cb90eb85070996560ed66e33fb6ff866
Chris <chris.haack@me.com>
2016-09-10 16:16:11 -0400

    whoops

--- spec/javascripts/e2e/login/facebook_login.js
+++ spec/javascripts/e2e/login/facebook_login.js
@@ -3,7 +3,7 @@ describe('logging in with Faceboook', function() {
 
   beforeEach(function() {
     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 70000;
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
   });
 
   afterEach(function() {
@@ -31,23 +31,23 @@ describe('logging in with Faceboook', function() {
       // switch to facebook auth popup and fill out form
       browser.switchTo().window(handles[1]).then(function(){
         browser.ignoreSynchronization = true;
-        browser.pause()
-        var email = element(by.css('input[type=email]'))
-        var password = element(by.css('input[type=password]'))
-        var submit = element(by.css('input[type=submit]'))
-        expect((email).isDisplayed()).toBeTruthy();
+        var email = element(by.id('email'))
+        var password = element(by.id('pass'))
+        var submit = element(by.id('u_0_2'))
+        //expect((email).isDisplayed()).toBeTruthy();
 
         // enter email
         // enter password
-        email.sendKeys('chicagoeditor@gmail.com');
-        password.sendKeys('$(#havana22.poo)');
+        email.sendKeys('youremail');
+        password.sendKeys('yourpassword');
 
         submit.click();
       });
       browser.ignoreSynchronization = false;
       // switch back to app window
       browser.switchTo().window(handles[0]).then(function(){
-      
+        // wait until favorites are visible as indicator that user is signed in
+        browser.driver.wait(protractor.until.elementIsVisible(favorites));
         expect((sign_in_with_facebook).isDisplayed()).toBeFalsy();
         expect((basic_login).isDisplayed()).toBeFalsy();
         expect((register).isDisplayed()).toBeFalsy();

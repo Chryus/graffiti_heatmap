328e2d6550d24e7ff5bbd4362ad74b9e247008c2
Chris <chris.haack@me.com>
2016-09-13 19:32:34 -0400

    add image uploader job

---
+++ app/jobs/image_uploader_job.rb
@@ -0,0 +1,55 @@
+class graffiti::ImageUploaderJob < ActiveJob::Base
+  queue_as :order_image
+
+  def perform(graffito)
+    @graffito = graffito
+    FileUtils.mkdir_p(cache_dir, mode: 0777)
+    graffito.files.each_with_index do |image, index|
+      process(image, 640, 480, "original")
+      process(image, 230, 140, "small")
+      process(image, 100, 100, "thumb")
+      FileUtils.rm image["tempfile"] # remove tempfile from disk
+      image.delete("tempfile") # delete tempfile pointer from db
+      if graffito.files.count-1 == index # if last image, write to s3
+        write_to_s3
+        graffito.update_attributes(encoded: true)
+        FileUtils.rm_r cache_dir # remove processed files from disk
+      end
+    end
+  end
+
+  def process(image, width, height, name)
+    Dir.glob(image["tempfile"]) do |file|
+      img = Magick::Image::read(file).first.resize_to_fit(width, height)
+      target = Magick::Image.new(width, height) do
+        self.background_color = 'black'
+      end
+      target.composite(img, Magick::CenterGravity, Magick::AtopCompositeOp).write("#{cache_dir}/#{name}_#{image['uuid']}.jpg") do
+        self.format = "JPEG"
+        self.quality = 90
+        self.compression = Magick::JPEGCompression
+      end
+    end
+  end
+
+  protected
+
+  def cache_dir
+    "tmp/uploads/graffiti/#{@graffito.order.id}/images/"
+  end
+
+  def s3_bucket
+    "mentor-marketplace-graffitos"
+  end
+
+  def s3_connection
+    @s3_connection ||= FogService.create
+  end
+
+  def write_to_s3
+    Dir.glob("#{cache_dir}/*.*") do |file|
+      destination = s3_connection.directories.get(s3_bucket)
+      destination.files.create(key: file.split('/').last, body: File.read(file))
+    end
+  end
+end

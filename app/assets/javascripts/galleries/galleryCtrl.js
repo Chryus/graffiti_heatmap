f404e5694d75e9aef34912cebd780f2c3542cb58
Chris <chris.haack@me.com>
2016-09-19 13:13:50 -0400

    fix dynamic generation of links; add styling

--- app/assets/javascripts/galleries/galleryCtrl.js
+++ app/assets/javascripts/galleries/galleryCtrl.js
@@ -6,17 +6,12 @@ angular.module('graffitiApp')
     function($scope, $location, users){
       $scope.currentPath = $location.path();
       $scope.user = users.user;
-      debugger
       $scope.__images = $scope.user.graffiti_images;
       $scope.base_url = "http://graffiti-image-uploads.s3.amazonaws.com"
       $scope.handleClick = function (event) {
         event.preventDefault();
         blueimp.Gallery(
-          document.getElementById('links').getElementsByTagName('a'),
-          {
-            container: '#blueimp-gallery',
-            carousel: true
-          }
+          $('#links a'), $('#blueimp-gallery').data()
         );
       };
   }])

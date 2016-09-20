431b378ea1a90f4403f44a98d20f1bdef7aa98d5
Chris <chris.haack@me.com>
2016-09-20 14:55:49 -0400

    refactor initializing carousel the angular way - $scope.watch

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -15,42 +15,31 @@ angular.module('graffitiApp')
       
       graffiti.getArchive().then( function ( response ) {
         $scope.__images = response.data;
-         $scope.$broadcast("imageLoaded");
-      }, function (response) {
-        alert("error");
-      })
+      })  
 
-      // open carousel when doc is ready
-      $scope.$on("imageLoaded", function(){
-        setTimeout(function(){
-          blueimp.Gallery(
-          $('#links a'), $('#blueimp-gallery').data(),
-          {
-            container: '#blueimp-gallery',
-            carousel: true,
-            startSlideshow: true,
-            slideshowInterval: 1000
-          }
-        );
-        }, 400);
-      })
+      // wait for images to load and init carousel
+      $scope.$watch(function() { return angular.element($('img')).is(':hidden') }, function() {
+        gallery = blueimp.Gallery($('#links a'), $('#blueimp-gallery').data());
+        gallery.play();
+        gallery.options['slideshowInterval'] = 2000
+      });
 
       //main data grab
-      // if (graffiti.graffiti.length == 0) {
-      //   graffiti.getAll().then( function ( response ) {
-      //     $scope.graffiti = response.data['graffiti'];
-      //     $scope.map = map.getMap('google');
-      //   }, function (response) {
-      //     alert("error");
-      //   }).finally(function() {
-      //     // called no matter success or failure
-      //     $('.close').click();
-      //     $scope.loading = false;
-      //   });
-      // } else {
-      //   $scope.graffiti = graffiti.graffiti
-      //   $scope.loading = false;
-      // }
+      if (graffiti.graffiti.length == 0) {
+        graffiti.getAll().then( function ( response ) {
+          $scope.graffiti = response.data['graffiti'];
+          $scope.map = map.getMap('google');
+        }, function (response) {
+          alert("error");
+        }).finally(function() {
+          // called no matter success or failure
+          $('.close').click();
+          $scope.loading = false;
+        });
+      } else {
+        $scope.graffiti = graffiti.graffiti
+        $scope.loading = false;
+      }
 
       $scope.matchLat = function (lat) {
         map.matchLat(lat);

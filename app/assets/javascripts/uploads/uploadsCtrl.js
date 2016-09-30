0da6a79cc77f38d343085fdf19d69ad732c3c4df
Chris <chris.haack@me.com>
2016-09-29 22:38:18 -0400

    set options the angular way

--- app/assets/javascripts/uploads/uploadsCtrl.js
+++ app/assets/javascripts/uploads/uploadsCtrl.js
@@ -14,26 +14,18 @@ angular.module('graffitiApp')
       graffiti.getS3DirectPost().then( function ( response ) {
         $scope.s3_direct_post = response.data.s3_direct_post;
         $scope.s3_direct_post_host = response.data.s3_direct_post_host;
+        $scope.options = {
+          paramName: 'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
+          dataType: 'XML',  // S3 returns XML if success_action_status is set to 201
+          type: 'POST',
+          url: $scope.s3_direct_post.url,
+          formData: $scope.s3_direct_post.fields,
+          acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
+        }
       }, function (response) {
         console.log("error getting s3 direct post");
       })
-
-
-      //$scope.host
-    //   fileInput.fileupload({
-    //   fileInput:       fileInput,
-    //   url:             form.data('url'),
-    //   type:            'POST',
-    //   autoUpload:       true,
-    //   formData:         form.data('form-data'),
-    //   paramName:        'file', // S3 does not like nested name fields i.e. name="user[avatar_url]"
-    //   dataType:         'XML',  // S3 returns XML if success_action_status is set to 201
-    //   replaceFileInput: false
-    // });
-      $scope.options = {
-        dataType: 'json',
-        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
-      }
+      
       $scope.$on('fileuploadstop', function(e, data){
         if ((Auth.isAuthenticated() == true || $auth.isAuthenticated() == true)) { 
           $state.go('gallery'); 
